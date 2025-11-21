5bda6721ce5d18468144e9f7961f381d3b37187c
Charly <me@macinsight.net>
2025-11-21 23:33:05 +0100

    Update

---
+++ home/features/cli/zsh.nix
@@ -0,0 +1,24 @@
+{config,lib,pkgs,...}: {
+  with lib; let
+  cfg = config.features.cli.zsh;
+  in {
+  options.features.cli.zsh.enable = mkEnableOption "Enable extended zsh configuration";
+  config = mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      enableCompletions = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        ls = "eza";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+        theme = "agnoster";
+      };
+      };
+    };
+  };
+}
+}
