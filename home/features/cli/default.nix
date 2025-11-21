30a59dcb30ccbba3f54c3c34b670fdc6ca8b131a
Charly <me@macinsight.net>
2025-11-21 22:57:22 +0100

    Update

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}: {
   programs.eza = {
     enable = true;
-    extraOptions = ["-l" "--icons" "--git" "-a"]
+    extraOptions = ["-l" "--icons" "--git" "-a"];
   };
   programs.bat = {enable=true;}
   home.packages = with pkgs; {
     coreutils
     fd
     htop
+    jq
     procs
     ripgrep
     tldr
