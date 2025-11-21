e1150af1ea8ae12b855183b8081d601698e50eb6
Charly <me@macinsight.net>
2025-11-21 21:55:03 +0100

    foo

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         "charly@artemis" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages."x86_64-linux";
           extraSpecialArgs = { inherit inputs outputs; };
-          modules = [ ./home/your-name/artemis.nix ];
+          modules = [ ./home/charly/artemis.nix ];
         };
       };
     };
