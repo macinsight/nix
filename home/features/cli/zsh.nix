23e04e204bfc7b3e61d83152eabee6b1dcfdb89b
Charly <me@macinsight.net>
2025-11-21 23:56:58 +0100

    Update

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -1,8 +1,13 @@
-{config, lib, pkgs,...}: {
-  with lib; let
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
   cfg = config.features.cli.zsh;
-  in {
+in
+{
   options.features.cli.zsh.enable = mkEnableOption "Enable extended zsh configuration";
+
   config = mkIf cfg.enable {
     programs.zsh = {
       enable = true;
@@ -16,7 +21,6 @@
         enable = true;
         plugins = [ "git" ];
         theme = "agnoster";
-      };
       };
     };
   };
