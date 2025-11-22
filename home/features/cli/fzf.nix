63c8de58d4b359082ca87c4dd400d0d3acf1f305
Charly <me@macinsight.net>
2025-11-22 10:51:33 +0100

    Update

---
+++ home/features/cli/fzf.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ...}: {
+  with lib; let
+  cfg = config.features.cli.fzf;
+  in {
+  options.features.cli.fzf.enable = mkEnableOption "Enable fzf";
+
+  config = mkIf cfg.enable {
+  programs.fzf = {
+    enable = true;
+
+  };
+  };
+};
+}
