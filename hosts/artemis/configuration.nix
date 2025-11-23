d3a2902cada166b8f46f0523bd6eeddd1b4573aa
Charly <charly@macinsight.net>
2025-11-23 15:33:24 +0100

    Disable wpa_supplicant, replace with networkmanager

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -17,14 +17,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "artemis"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  #  networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
