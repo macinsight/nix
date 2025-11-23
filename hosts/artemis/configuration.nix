8c7259797e486821242190be0cbc7a69dd6ef9b1
Charly <charly@macinsight.net>
2025-11-23 16:15:18 +0100

    foo

--- hosts/artemis/configuration.nix
+++ hosts/artemis/configuration.nix
@@ -96,4 +96,30 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
   nix.settings.require-sigs = false;
+
+	hardware.bluetooth = {
+	  enable = true;
+	  powerOnBoot = true;
+	  settings = {
+	    General = {
+	      # Shows battery charge of connected devices on supported
+	      # Bluetooth adapters. Defaults to 'false'.
+	      Experimental = true;
+	      # When enabled other devices can connect faster to us, however
+	      # the tradeoff is increased power consumption. Defaults to
+	      # 'false'.
+	      FastConnectable = true;
+	    };
+	    Policy = {
+	      # Enable all controllers when they are found. This includes
+	      # adapters present on start as well as adapters that are plugged
+	      # in later on. Defaults to 'true'.
+	      AutoEnable = true;
+	    };
+	  };
+	};
+
+
+
+
 }
