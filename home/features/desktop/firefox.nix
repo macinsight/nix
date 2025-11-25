b828540b38fce6568bb902ceb788da2af9575e07
Charly <charly@macinsight.net>
2025-11-25 10:47:12 +0100

    foo

--- home/features/desktop/firefox.nix
+++ home/features/desktop/firefox.nix
@@ -1,11 +1,43 @@
-{ config, lib, ...}:
-with lib; let
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
   cfg = config.features.desktop.firefox;
-in {
-  options.features.desktop.firefox.enable = mkEnableOption "firefox config";
+in
+{
+  options.features.desktop.firefox.enable =
+    mkEnableOption "firefox config";
+
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+
+      profiles = {
+        default = {
+          id = 0;
+          name = "default";
+
+          extensions = {
+            packages = with nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              stylus
+              bitwarden
+            ];
+          };
+
+          bookmarks = {
+            settings = [
+              {
+                name    = "wikipedia";
+                tags    = [ "wiki" ];
+                keyword = "wiki";
+                url     = "https://wikipedia.org/";
+              }
+            ];
+          };
+        };
+      };
     };
   };
 }
+
