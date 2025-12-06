8d25b015e6f511c1bd409a530a160803ca309a03
Charly <charly@macinsight.net>
2025-12-06 17:12:28 +0100

    foo

--- home/charly/home.nix
+++ home/charly/home.nix
@@ -1,25 +1,54 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-{
-  home.username = lib.mkDefault "charly";
-  home.homeDirectory = lib.mkDefault "/home/${config.home.username}";
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-  home.packages = with pkgs; [
-    kitty
-    wofi
-    ashell
-    unzip
-    bitwarden-cli
-    swww
-    waypaper
-  ];
-  home.sessionVariables = {
-  };
-
-## Services
-services.vdirsyncer.enable = true;
-
-
-  # Let HM manage itself
+{ # HM Management Stuff
+  home.username = "charly";
+  home.homeDirectory = "/home/charly";
+  home.stateVersion = "25.05"; # Please read the comment before changing.
   programs.home-manager.enable = true;
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  
+  # Package management
+  home.packages = [
+  ];
+ programs.neovim = {
+	 enable = true;
+	 package = pkgs.neovim-nightly;
+	 vimAlias = true;
+	 vimdiffAlias = true;
+	 withNodeJs = true;
+ 	plugins = with pkgs.vimPlugins; [
+	lazy-nvim
+	which-key-nvim
+	];
+	
+  extraLuaConfig = ''
+    vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+    require("lazy").setup({
+    spec = {
+{ import = "plugins" },
+    },
+      performance = {
+        reset_packpath = false,
+        rtp = {
+            reset = false,
+          }
+        },
+      dev = {
+        path = "${pkgs.vimUtils.packDir config.home-manager.users.charly.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+	patterns = {""},
+      },
+      install = {
+        -- Safeguard in case we forget to install a plugin with Nix
+        missing = false,
+      },
+    })
+  '';
+ };
+
+ xdg.configFile."nvim/lua" = {
+ recursive = true;
+ soutce = ./lua;
+ };
 }
